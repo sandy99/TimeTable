a6a2600ec2e6f108e660a736de392c6d05751f32
“sandy99” <sandeepsharma638@gmail.com>
2019-03-07 11:51:57 +0530

    temp01

--- TimeTable/ViewController.swift
+++ TimeTable/ViewController.swift
@@ -26,6 +26,11 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         pickerView2.selectRow(4, inComponent: 0, animated: false)
         labelResult.text = "16"
         timeTable = TimeTable()
+        
+        print("test")
+        print("test")
+        print("test")
+
     }
 
     override func didReceiveMemoryWarning() {
